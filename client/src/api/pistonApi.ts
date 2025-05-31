af6b88b5751148681c5e5390a05e7d29b7b69a91
Shruti jadaun <shrutijadaun@outlook.com>
2025-05-31 15:02:53 +0530

    up

--- client/src/api/pistonApi.ts
+++ client/src/api/pistonApi.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance } from "axios"
 
+export const url = "https://code-sync-1-9gig.onrender.com"
 const pistonBaseUrl = "https://emkc.org/api/v2/piston"
 
 const instance: AxiosInstance = axios.create({
