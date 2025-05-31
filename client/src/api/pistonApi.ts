4396360095c9e308a559c5dd58033d9e5e193332
Shruti jadaun <shrutijadaun@outlook.com>
2025-05-31 17:52:49 +0530

    url

--- client/src/api/pistonApi.ts
+++ client/src/api/pistonApi.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from "axios"
-
+export const url = "https://code-sync-orpin-two.vercel.app/"
 {/*export const url = "https://code-sync-1-9gig.onrender.com"*/}
 const pistonBaseUrl = "https://emkc.org/api/v2/piston"
 
