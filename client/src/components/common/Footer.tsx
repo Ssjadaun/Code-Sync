75fea6c55ec8f258d8d108e68ace8341c5737e54
Shruti jadaun <shrutijadaun@outlook.com>
2025-05-31 17:34:05 +0530

    users

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -2,9 +2,9 @@ function Footer() {
     return (
         <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
             <span>
-                Build with ❤️ by{" "}
+                Build with ❤️ by{"Ssjadaun"}
                 <a
-                    href="https://github.com/sahilatahar"
+                    href="https://github.com/Ssjadaun/Code-Sync"
                     className="text-primary underline underline-offset-1"
                 >
                     sahilatahar
